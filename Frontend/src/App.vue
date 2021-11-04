7e2d64b462890082a5d852b23affa1a032d71c58
Julien Thiphineau <j.thiphineau@yahoo.fr>
2021-11-04 14:42:28 +0100

    recup Frontend Synology

---
+++ Frontend/src/App.vue
@@ -0,0 +1,26 @@
+<!-- fichier central de l'qpplication Vue.-->
+
+<template>
+    <div id="app">
+      <Navi></Navi>
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Navi from "./components/Navi";
+
+export default {
+    name: "App",
+    components: {
+      Navi
+    }
+}
+</script>
+
+<style>
+  body {
+    background: #EB5A77;
+    background: linear-gradient(90deg, #EB5A77 0%, #ec254c 100%);
+  }
+</style>
